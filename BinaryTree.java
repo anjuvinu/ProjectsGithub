bff6da2191201609d941a2ac910350c574a5ce60
Anju Varghese <anjuelavathingal@gmail.com>
2025-12-09 14:24:43 -0600

    BinaryTreeDFS

---
+++ BinaryTree.java
@@ -0,0 +1,28 @@
+public class BinaryTree {
+
+  //Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ //DFS approach to find the maximum depth of a binary tree
+  
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftDepth =maxDepth(root.left);
+        int rightDepth =maxDepth(root.right);
+        return 1+ Math.max(leftDepth,rightDepth);
+    }
+}
+}
